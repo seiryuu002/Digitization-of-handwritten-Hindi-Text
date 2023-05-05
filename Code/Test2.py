fcbd1e036504865a9a26f65e9c3876bfcdeeff10
Apurv Kumar <kuroseiryuu025@gmail.com>
2023-05-05 09:54:55 +0530

    update

--- Code/Test2.py
+++ Code/Test2.py
@@ -7,14 +7,14 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-file = "F:\\Lecture notes\\Sem-8\\MP(8)\\Resources\\letters.jpeg"
+file = "F:\\Lecture notes\\Sem-8\\MP(8)\\Resources\\Handwriting3.jpg"
 img =cv2.imread(file)
 # img = cv2.resize(img, None, fx= 0.3, fy= 0.3) 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 11)
-# img = cv2.drawContours(img)
-# print(pytesseract.image_to_string(img, lang= 'hin'))
+
+print(pytesseract.image_to_string(img, lang= 'hin'))
 
 cv2.imshow("image", img)
 cv2.waitKey(0)
